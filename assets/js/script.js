9f18e1150a87715b26d23ea45b782264a82d367d
joel57 <67278919+joel57@users.noreply.github.com>
2022-07-31 15:35:53 -0700

    almost finished with the html

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {// tells engine to load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+    //assign saveBtn click listener for user input and time stamp??
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+   
