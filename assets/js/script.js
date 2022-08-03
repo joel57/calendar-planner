0e23c7ca0e03d1ef5af8d214d8ffb06da8f65c6e
joel57 <67278919+joel57@users.noreply.github.com>
2022-08-02 18:45:49 -0700

    added the localstorage pulling for the time

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,15 @@ $(document).ready(function () {// tells engine to load 1)html & 2)css first.
         //set items in local storage.
         localStorage.setItem(time, text);
     })
-
+    
+//load any saved data from LocalStorage - do this for each hour created. Should follow html 24 hour to 12 hour conversion.
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
