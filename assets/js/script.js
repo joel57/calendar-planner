c566c811da6f772374f7ef04c15a3215a657b373
joel57 <67278919+joel57@users.noreply.github.com>
2022-08-08 21:29:21 -0700

    finished up the js and fixes issue with the time element.

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,25 @@ function hourTracker() {
     $(".time-block").each(function () {
         var blockHour = parseInt($(this).attr("id").split("hour")[1]);
         console.log( blockHour, currentHour)
+
+ //check if we've moved past this time, click into css/html given classes of past, present, or future
+ if (blockHour < currentHour) {
+    $(this).addClass("past");
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+}
+else if (blockHour === currentHour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+    $(this).removeClass("future");
+}
+else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+}
+})
+}
+hourTracker(); //re-run function
+})
+
