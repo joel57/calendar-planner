271503ac36cc82209120a4bbb045262147eee48f
joel57 <67278919+joel57@users.noreply.github.com>
2022-08-09 18:40:26 -0700

    updates to the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-$(document).ready(function () {// tells engine to load 1)html & 2)css first.
+$(document).ready(function () {
+    // this tells the machine to load 1)html & 2)css first.
     //display current day & time.
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
+    // use of moment.js
     //assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
         //get nearby values.
         console.log(this);
-        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+        var text = $(this).siblings(".description").val(); 
+        // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); 
+        // taken the change from the parent html id attribute
 
         //set items in local storage.
         localStorage.setItem(time, text);
@@ -33,24 +37,25 @@ function hourTracker() {
         var blockHour = parseInt($(this).attr("id").split("hour")[1]);
         console.log( blockHour, currentHour)
 
- //check if we've moved past this time, click into css/html given classes of past, present, or future
- if (blockHour < currentHour) {
-    $(this).addClass("past");
-    $(this).removeClass("future");
-    $(this).removeClass("present");
-}
-else if (blockHour === currentHour) {
-    $(this).removeClass("past");
-    $(this).addClass("present");
-    $(this).removeClass("future");
-}
-else {
-    $(this).removeClass("present");
-    $(this).removeClass("past");
-    $(this).addClass("future");
-}
-})
-}
-hourTracker(); //re-run function
+    //check if we've moved past this time, click into css/html given classes of past, present, or future
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    }
+    else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+    })
+    }
+hourTracker(); 
+//re-run function
 })
 
